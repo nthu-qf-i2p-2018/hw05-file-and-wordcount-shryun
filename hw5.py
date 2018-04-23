e7712436ce8f016bf5a085f839009557fb460cf7
106195033 <37100591+shryun@users.noreply.github.com>
2018-04-24 00:29:46 +0800

    Add files via upload

--- hw5.py
+++ hw5.py
@@ -1,48 +1,80 @@
 # -*- coding: utf-8 -*-
-import ...
+import sys
 
 
 def main(filename):
     # read file into lines
-    lines = ...
+    txtfile = open(filename,"r")
+    lines = txtfile.readlines()
+
+    
+
 
     # declare a word list
     all_words = []
-
+    words=[]
     # extract all words from lines
     for line in lines:
         # split a line of text into a list words
         # "I have a dream." => ["I", "have", "a", "dream."]
-        words = ...
+        words.append(line.split())
 
+        
         # check the format of words and append it to "all_words" list
-        for word in words:
-            # then, remove (strip) unwanted punctuations from every word
-            # "dream." => "dream"
-            word = ...
-            # check if word is not empty
-            if word:
+    for word in words:
+        # then, remove (strip) unwanted punctuations from every word
+        # "dream." => "dream"
+        for a in word:
+        # check if word is not empty
+            if len(a)==0:
+                continue
+            else:
+                a=a.strip(',.!-:"?')
+                if len(a)!=0:
+                    all_words.append(a)
                 # append the word to "all_words" list
-                all_words...
 
     # compute word count from all_words
-    counter = ...
+    print(len(all_words))
+    count={}
+    for data in all_words:
+        if data not in count:
+            count[data]=1
+        else:
+            count[data]=count[data]+1
+
+    
+
 
     # dump to a csv file named "wordcount.csv":
     # word,count
     # a,12345
     # I,23456
     # ...
-    with open(...) as csv_file:
+    import csv
+    with open('wordcount.csv','w',newline='') as csv_file:
         # create a csv writer from a file object (or descriptor)
-        writer = ...
+        writer = csv.writer(csv_file, dialect='excel')
         # write table head
         writer.writerow(['word', 'count'])
         # write all (word, count) pair into the csv writer
-        writer.writerows(...)
+        for a in count:
+            writer.writerow([a,count[a]])
+
+
+    list1=[]
+
+    for a in count:
+        temp=[]
+        temp=[a,count[a]]
+        list1.append(temp)
+
+    print(list1)
 
     # dump to a json file named "wordcount.json"
-    ...
+    import json
+    f=open('wordcount.json','w')
+    json.dump(list1,f)
 
     # BONUS: dump to a pickle file named "wordcount.pkl"
     # hint: dump the Counter object directly
